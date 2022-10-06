54409b16879aee337e991b179d07fdfcb33d778e
Freddy <you@example.com>
2022-10-06 17:46:14 -0300

    tercero5

--- apiDjango/settings.py
+++ apiDjango/settings.py
@@ -110,19 +110,21 @@ WSGI_APPLICATION = 'apiDjango.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'ejemploapi',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+      #  'default': {
+      #  'ENGINE': 'django.db.backends.mysql', 
+      #  'NAME': 'ejemploapi',
+      #  'USER': 'root',
+      #  'PASSWORD': '',
+      #  'HOST': 'localhost',
+      #  'PORT': '3306',
     
-   # 'default': {
-   #     'ENGINE': 'django.db.backends.sqlite3',
-   #     'NAME': BASE_DIR / 'db.sqlite3',
+    
    # }
+ 'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+   
     
 }
 
@@ -167,5 +169,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+import django_heroku
 django_heroku.settings(locals())
